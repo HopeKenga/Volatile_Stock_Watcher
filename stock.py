a69088e27e57c8ab2af9d5e7ce6e40e94c0ac77a
Hope Kenga <kengahope7@gmail.com>
2023-04-13 15:45:06 +0300

    chore: first commit

---
+++ stock.py
@@ -0,0 +1,23 @@
+"""
+Use htpps:finnhub.io free stock price API to query stock price 
+for specific tech stocks. Use the `requests` package in Python.
+"""
+
+import requests
+import csv
+
+csv_header =[]
+volatile_stock_dict =dict()
+most_volatile_stock = [] #declared as an array to get the max value of it
+
+def get_stock_price(stock_symbol) : #destructuring. Asssigning return values to the declared method
+     data, response = make_request(stock_symbol)
+     return data['c'] if response.status_code == 200 else 'Access Denied'
+
+"""
+Get the lates price for Apple, Netflix, Facebook, Google
+"""
+
+def get_latest_price(list_of_stocks):
+    stock_prices ={}
+    #for every item in the list, look up it's price
